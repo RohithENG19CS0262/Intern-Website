43b9c26890941d68aaa240bc96079ef9695d16d5
Rohith.S <76095921+RohithENG19CS0262@users.noreply.github.com>
2021-11-13 12:09:56 +0530

    Update Connecting DB.js

--- Connecting DB.js
+++ Connecting DB.js
@@ -1,11 +1,11 @@
 var express = require('express');
-var path = require('C:\Users\Tousif\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\MongoDB Inc');
+var path = require('C:/Users/Tousif/Downloads/Others');
 var mongodb = require('mongodb');
 
 var dbConn = mongodb.MongoClient.connect('mongodb://localhost:27017');
 
 var app = express();
 
-app.use(express.static(path.resolve(__dirname, 'Landing page.html')));
+app.use(express.static(path.resolve(__dirname, 'Intern.html')));
 
 app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0' );
