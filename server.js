d29715644abcf60f507771fe22f841484d9acea8
Rohith.S <76095921+RohithENG19CS0262@users.noreply.github.com>
2021-11-13 12:03:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var path = require('C:\Users\Tousif\node_modules\express\lib\express.js');
 
 var app = express();
 
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, 'Landing page.html')));
 
 app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0' );
