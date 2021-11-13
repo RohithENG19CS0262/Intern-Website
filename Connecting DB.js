f33fa37bc72dcf392ec55bb49d01e39eb45f5c5f
Rohith.S <76095921+RohithENG19CS0262@users.noreply.github.com>
2021-11-13 12:04:00 +0530

    Update Connecting DB.js

--- Connecting DB.js
+++ Connecting DB.js
@@ -6,6 +6,6 @@ var dbConn = mongodb.MongoClient.connect('mongodb://localhost:27017');
 
 var app = express();
 
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, 'Landing page.html')));
 
 app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0' );
