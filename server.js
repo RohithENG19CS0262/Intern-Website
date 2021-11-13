be89ef20763ece00018932750798ff9c73cb43f7
Rohith.S <76095921+RohithENG19CS0262@users.noreply.github.com>
2021-11-13 12:10:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var path = require('C:\Users\Tousif\node_modules\express\lib\express.js');
+var path = require('C:/Users/Tousif/Downloads/Others');
 
 var app = express();
 
-app.use(express.static(path.resolve(__dirname, 'Landing page.html')));
+app.use(express.static(path.resolve(__dirname, 'Intern.html')));
 
 app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0' );
